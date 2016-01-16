59908f749e3c3d715d4e5b5f62b7ece0fece81a4
morgan-ashley <morgan.angela.ashley@gmail.com>
2016-01-15 22:30:54 -0800

    Update and style
    Updated JSON array of locations, fixed AJAX request, added Foursquare
    logo at footer, applied toggle drop down menu, started implementing
    media queries.

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 * http://www.knockmeout.net/2011/04/utility-functions-in-knockoutjs.html
 * http://www.evoluted.net/thinktank/web-design/custom-google-maps-style-tool
 * https://color.adobe.com/Beach-Time-color-theme-2629293/ (I chose this theme because it reminds me of the Painted Ladies at Alamo Square Park)
+* https://medium.com/@jrdnndtsch/drop-down-hamburger-menu-with-jquery-1ae7fa572c04#.prhyr3t6r
 
 ###Frameworks and APIS###
 * HTML, CSS, JS
--- js/app.js
+++ js/app.js
@@ -1,195 +1,108 @@
 var infowindow, map, marker;
 
 var initialLocations = [{
-	"id": "44e37004f964a52042371fe3",
-	"name": "Bar Crudo",
-	"latLng": {lat: 37.775685, lng: -122.438223},
-  "marker": null
+  "name": "Nopa",
+  "latLng": {lat: 37.775188, lng: -122.437461},
+},  
+{
+  "name": "Bar Crudo",
+  "latLng": {lat: 37.775685, lng: -122.438223},
 }, 
 {
-	"id": "53b4dd52498eef48c82d89a5",
-	"name": "Monsieur Benjamin",
-	"latLng": {lat: 37.777504, lng: -122.423334},
-  "marker": null
-}, 
+  "name": "Barrel Head Brewhouse",
+  "latLng": {lat: 37.775676, lng: -122.446110},
+},
 {
-	"id": "44646408f964a52026331fe3",
-	"name": "Nopa",
-	"latLng": {lat: 37.775188, lng: -122.437461},
-  "marker": null
-}, 
-{
-	"id": "43dc103ef964a520992e1fe3",
-	"name": "Absinthe Brasserie & Bar",
-	"latLng": {lat: 37.777035, lng: -122.422923},
-  "marker": null  
-}, 
-{
-	"id": "49be8fe0f964a520b9541fe3",
-	"name": "Nopalito",
-	"latLng": {lat: 37.773564, lng: -122.438857},
-  "marker": null
-}, 
-{
-	"id": "4afe6db4f964a520682f22e3",
-	"name": "Smuggler's Cove",
-	"latLng": {lat: 37.779432, lng: -122.423352},
-  "marker": null
-}, 
-{
-	"id": "4baae1f9f964a520f18b3ae3",
 	"name": "Chile Pies & Ice Cream",
 	"latLng": {lat: 37.776578, lng: -122.441692},
-  "marker": null
 }, 
-{
-	"id": "433dd180f964a52048281fe3",  
+{  
 	"name": "Little Star Pizza",
 	"latLng": {lat: 37.777539, lng: -122.438015},
-  "marker": null
-}, 
+},
 {
-	"id": "4e937514f9f44dd023514f2b",
-	"name": "Two Sisters Bar and Books",
-	"latLng": {lat: 37.776363, lng: -122.425833}, 
-  "marker": null
-}, 
+  "name": "Bistro Central Parc",
+  "latLng": {lat:  37.775013, lng: -122.444338},
+},  
 {
-	"id": "4feddd79d86cd6f22dc171a9",
 	"name": "The Mill",
 	"latLng": {lat: 37.776469, lng: -122.437792}, 
-  "marker": null
 }, 
 {
-	"id": "53644323498e8964add3b940",
+  "name": "Nopalito",
+  "latLng": {lat: 37.773564, lng: -122.438857},
+}, 
+{
 	"name": "4505 Burgers & BBQ",
-	"latLng": {lat: 37.776207, lng: -122.438252}, 
-  "marker": null
+	"latLng": {lat: 37.776207, lng: -122.438252},
 }]
 
 function Place(data) {
     this.name = data.name;
     this.latLng = data.latLng;  
-    this.id= data.id;
     this.marker = ko.observable(data.marker);
-   // this.street = ko.observable();
-   // this.city = ko.observable();
   }
 
 var ViewModel = function() {
   var self = this;
-  var styles = [
+  
+/* Styles for Google Map */  
+var styles = [
   {
     "featureType": "landscape",
     "stylers": [
-      {
-        "hue": "#00FF8A"
-      },
-      {
-        "saturation": -27.272727272727266
-      },
-      {
-        "lightness": -16.39215686274511
-      },
-      {
-        "gamma": 1
-      }
+      {"hue": "#00FF8A"},
+      {"saturation": -27.272727272727266},
+      {"lightness": -16.39215686274511},
+      {"gamma": 1}
     ]
   },
   {
     "featureType": "road.highway",
     "stylers": [
-      {
-        "hue": "#FFB200"
-      },
-      {
-        "saturation": 100
-      },
-      {
-        "lightness": -2.521568627450975
-      },
-      {
-        "gamma": 1
-      }
-    ]
-  },
-  {
-    "featureType": "road.arterial",
-    "stylers": [
-      {
-        "hue": "#FBFF00"
-      },
-      {
-        "saturation": 0
-      },
-      {
-        "lightness": 0
-      },
-      {
-        "gamma": 1
-      }
+      {"hue": "#FF0D00"},
+      {"saturation": 100},
+      {"lightness": -12.721568627450978},
+      {"gamma": 1}
     ]
   },
   {
     "featureType": "road.local",
     "stylers": [
-      {
-        "hue": "#00FF8A"
-      },
-      {
-        "saturation": -27.272727272727266
-      },
-      {
-        "lightness": -8.39215686274511
-      },
-      {
-        "gamma": 1
-      }
+      {"hue": "#FFD000"},
+      {"saturation": 100},
+      {"lightness": 19.84313725490196},
+      {"gamma": 1}
     ]
   },
   {
     "featureType": "water",
     "stylers": [
-      {
-        "hue": "#FF0300"
-      },
-      {
-        "saturation": -100
-      },
-      {
-        "lightness": 148
-      },
-      {
-        "gamma": 1
-      }
+      {"hue": "#FF0300"},
+      {"saturation": -100},
+      {"lightness": 148},
+      {"gamma": 1}
     ]
   },
   {
     "featureType": "poi",
     "stylers": [
-      {
-        "hue": "#FFCD00"
-      },
-      {
-        "saturation": 100
-      },
-      {
-        "lightness": 19.84313725490196
-      },
-      {
-        "gamma": 1
-      }
+      {"hue": "#00FF23"},
+      {"saturation": -25.806451612903203},
+      {"lightness": 3.3725490196078454},
+      {"gamma": 1}
     ]
   }
 ];
+
   /* Links list view to marker when user clicks on the list element */
   self.itemClick = function(marker) {
         google.maps.event.trigger(this.marker, 'click');
       };
-  
+
   /* Create a new Google Map object */
   self.googleMap = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 37.776859, lng: -122.431403},
+    center: {lat: 37.775732, lng: -122.441672},
     zoom: 16,
     styles: styles,    
     zoomControl: true,
@@ -198,6 +111,8 @@ var ViewModel = function() {
           position: google.maps.ControlPosition.RIGHT_BOTTOM
         }
     });
+
+
   var contentString;
   /* Declare Google map info window */
   self.infowindow = new google.maps.InfoWindow({
@@ -219,42 +134,48 @@ var ViewModel = function() {
     };
    
     place.marker = new google.maps.Marker(markerOptions);
-    
-    /* Add click listener to marker and open info window */
-    place.marker.addListener('click', function(){
 
-      /* Set timeout animation */
-      place.marker.setAnimation(google.maps.Animation.BOUNCE);
-      setTimeout(function(){ place.marker.setAnimation(null); }, 2150);
-
-      /* Foursquare API */
+     /* Foursquare API */
       var clientID = "AWOF0E4HV3H1H3BFIGE2KGZA5F2PMKF2UEU3ZL0QFZTEJPTP";
       var clientSecret = "NLOWB4Z4KOE1KVJNZ5PXIZHUVS04RSQWJ5GTLDHLZO0QMMUE";
       var foursquareURL = 'https://api.foursquare.com/v2/venues/search?limit=1&ll=' + place.latLng.lat + ',' + place.latLng.lng + '&client_id=' + clientID + '&client_secret='+ clientSecret + '&v=20140806';
       var results, name, url, street, city;
      
       $.getJSON(foursquareURL, function(data){
+        console.log('AJAX is working!');
         results = data.response.venues[0],
-        //place.name = results.name,
-        place.name = results.hasOwnProperty('name') ? results.name : '';
-         console.dir(results.name),
+        place.name = results.name,
+       // place.name = results.hasOwnProperty('name') ? results.name : '';
+         //console.dir(results.name),
         place.url= results.hasOwnProperty('url') ? results.url : '';
-          console.dir(results.url),
-        //place.street = results.location.formattedAddress[0],
-        place.city = results.hasOwnProperty('formattedAddress') ? results.location.formattedAddress[0] : '',
-         console.dir(results.location.formattedAddress[0]),
-         //place.city = results.location.formattedAddress[1]
-        place.street = results.hasOwnProperty('formattedAddress') ? results.location.formattedAddress[1] : '',
-          console.dir(results.location.formattedAddress[1])
+          //console.dir(results.url),
+        place.street = results.location.formattedAddress[0],
+        //place.city = results.hasOwnProperty('formattedAddress') ? results.location.formattedAddress[0] : '',
+         //console.dir(results.location.formattedAddress[0]),
+         place.city = results.location.formattedAddress[1]
+        //place.street = results.hasOwnProperty('formattedAddress') ? results.location.formattedAddress[1] : '',
+          //console.dir(results.location.formattedAddress[1])
           
       /* error response */
       }).error(function(e){
         $('p').text('Woopsie Daisy! Looks like something went wrong!');
       });
-       contentString = '<h4>' + place.name + '</h4>\n<p><b>Address:</b></p>\n<p>' + place.street + '</p>\n<p>' + place.city + '</p><a href= ' + place.url + '>' + place.url + '</a>';   
+    
+    /* Add click listener to marker and open info window */
+    place.marker.addListener('click', function(){
+      console.log("hi, marker click");
+
+      /* Set timeout animation */
+      place.marker.setAnimation(google.maps.Animation.BOUNCE);
+      setTimeout(function(){ place.marker.setAnimation(null); }, 1400);
+
+      
+       contentString = '<h4>' + place.name + '</h4>\n<p>' + place.street + '</p>\n<p>' + place.city + '</p><a href= ' + place.url + '>' + place.url + '</a>';   
       /* Open info window and set its content */
+      console.log('opening infowindows');
       self.infowindow.setContent(contentString);
       self.infowindow.open(self.googleMap, place.marker);
+
     })
   });
 
